908124d899be80ccae56988a755a5e50a83659eb
Jay Qi <jayqi@users.noreply.github.com>
2024-11-11 18:47:48 -0500

    iamo-team explainability

---
+++ explainability/5th place/Rpackages.R
@@ -0,0 +1,11 @@
+####### Installing necessary R packages for data preprocessing and inference ##############
+
+packages <- c( "readr", "dplyr", "tidyr", "lubridate",'data.table',"terra", "reshape2",
+               'scales','doParallel','caret','pls', 'vip','ggplot2','knitr', 'pdp',
+               "gridExtra"
+             )
+
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages], repos = "http://cran.us.r-project.org")
+}
